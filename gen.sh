7248495c5b5aee1926f0224a9debe0f0f9f7068f
Declan Chidlow <git.rh92c@8shield.net>
2024-02-01 14:20:30 +0800

    Document README

--- gen.sh
+++ gen.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+# Generate pages
 adduce -c config/pages/index -n index.html -o docs
 adduce -c config/pages/demo -n demo.html -o docs
 adduce -c config/pages/404 -n 404.html -o docs
 
+# Copy required material
 cp -r config/global/style docs/
 cp -r config/global/assets docs/
