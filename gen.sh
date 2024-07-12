92d3c97dc3a6ab9473e4cbc7790ef3460c690e1f
Declan Chidlow <git.rh92c@8shield.net>
2024-07-12 09:50:06 +0800

    Update to reflect changes to Adduce

--- gen.sh
+++ gen.sh
@@ -2,19 +2,19 @@
 
 # Generate standard pages
 pages=(
-    "config/pages/index index.html"
-    "config/pages/downloads downloads.html"
-    "config/pages/releases releases.html"
-	"config/pages/404 404.html"
+	"input/pages/index index.html"
+	"input/pages/downloads downloads.html"
+	"input/pages/releases releases.html"
+	"input/pages/404 404.html"
 )
 
 for page_config in "${pages[@]}"; do
-    page="${page_config%% *}"
-    output="${page_config#* }"
-    adduce -c "$page" -n "$output" -o docs
+	page="${page_config%% *}"
+	output="${page_config#* }"
+	adduce -c "$page" -n "$output" -o docs
 done
 
 # Copy global styles and assets
-cp -r config/global/styles docs/
-cp -r config/global/assets docs/
-cp -r config/global/scripts docs/
+cp -r input/global/styles docs/
+cp -r input/global/assets docs/
+cp -r input/global/scripts docs/
